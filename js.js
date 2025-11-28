b7c684a6ef0f1ca2e335e23cb9bd59b0393da303
Yitzhak Valenzuela <yitzhak.1995.valenzuela@gmail.com>
2025-11-28 15:02:23 -0300

    supongo que si

---
+++ js.js
@@ -0,0 +1,9 @@
+let clima = "no se";
+
+if (clima == "soleado") {
+    console.log("¡Es un día perfecto para un picnic!");
+} else if (clima == "lluvioso") {
+    console.log("¡Hora de saltar en charcos!");
+} else {
+    console.log("¡Vamos a tirarnos en trineo en la nieve!");
+}
